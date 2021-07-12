829f934f83f36ab62e15e05086676d13bb44a60a
Lokesh Choubisa <lokeshchoubisa26@gmai.com>
2021-07-12 21:36:01 +0530

    added

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,3 +1,3 @@
-// export const api="https://mern-eccomerce-rest-server.herokuapp.com/api"
-export const api="http://localhost:3000/api"
+export const api="https://mern-eccomerce-rest-server.herokuapp.com/api"
+// export const api="http://localhost:3000/api"
 export const pictureUrl=''
